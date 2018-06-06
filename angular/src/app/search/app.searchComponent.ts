b33dc0712886e2bf64a8594543601e09b45e7572
huyentk <huyentk1296@gmail.com>
2018-06-06 20:59:30 +0700

    mess

--- angular/src/app/search/app.searchComponent.ts
+++ angular/src/app/search/app.searchComponent.ts
@@ -1,8 +1,8 @@
 import {Component} from '@angular/core';
-import {UserBehavior} from "../user-behavior";
-import {HttpClient} from "@angular/common/http";
-import {PouchdbService} from "../pouchdb.service";
-import {SolrService} from "../solr.service";
+import {UserBehavior} from '../user-behavior';
+import {HttpClient} from '@angular/common/http';
+import {PouchdbService} from '../pouchdb.service';
+import {SolrService} from '../solr.service';
 
 @Component({
   selector: 'search',
@@ -13,20 +13,31 @@ export class searchComponent {
   keyword = '';
   items = [];
   behavior = new UserBehavior(1);
-  message = '';
+  area = '';
+  cat = '';
 
   constructor(private http: HttpClient, private pouchdb: PouchdbService, private solr: SolrService) {
   }
 
   searchDocument() {
     this.items = [];
-    return this.solr.search(this.keyword).subscribe((res) => {
-
+    return this.solr.search(this.keyword, this.area, this.cat).subscribe((res) => {
+      Object.keys(res.response.docs).map(k => {
+        let doc = res.response.docs[k];
+        let title = doc.title[0];
+        let content = doc.description[0];
+        let price = doc.price[0];
+        this.items.push({
+          'title': title,
+          'content': content,
+          'price': price
+        });
+      });
     });
   }
 
   saveUserBehavior(item_id) {
     this.behavior.item = item_id;
-    // this.pouchdb.add(this.behavior);
+    this.pouchdb.add(this.behavior);
   }
 }
