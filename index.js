83c9e73af2d86557c2e98fbddfd8764c7c8a8da2
huyentk <huyentk1296@gmail.com>
2018-06-11 16:48:10 +0700

    Merge branch 'master' of https://github.com/tramnguyennt2/advertisement

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/angular/dist/'));
+app.use(express.static(__dirname + '/dist/'));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/angular/dist/index.html'));
