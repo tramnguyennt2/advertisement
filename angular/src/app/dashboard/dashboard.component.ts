1f26665eac9a98a33dd186624d967dd19d715790
huyentk <huyentk1296@gmail.com>
2018-08-07 19:51:05 +0700

    missing component

--- angular/src/app/dashboard/dashboard.component.ts
+++ angular/src/app/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {CategoryService} from "../services/category.service";
 
 @Component({
-  selector: 'app-content-area',
+  selector: 'app-dashboard-area',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
