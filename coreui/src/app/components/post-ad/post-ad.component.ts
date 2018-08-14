0d83b077bad903dbf631eeff746655571948a656
tramnguyen <tramnguyennt2@gmail.com>
2018-08-14 23:38:56 +0700

    Sửa giao diện trang đăng tin

--- coreui/src/app/components/post-ad/post-ad.component.ts
+++ coreui/src/app/components/post-ad/post-ad.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from "../../services/category.service";
+import locationsJson from "assets/data/location.json";
 
 @Component({
   selector: 'app-post-ad',
@@ -6,10 +8,81 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-ad.component.scss']
 })
 export class PostAdComponent implements OnInit {
+  cats = [];
+  locations = [];
+  category = '';
+  subCategory = 'Chọn danh mục';
+  location = '';
+  province = 'Chọn khu vực';
 
-  constructor() { }
+  constructor(private catService: CategoryService) { }
 
   ngOnInit() {
+    //load category
+    this.catService.getAllCategory().subscribe(cats => {
+      this.cats = cats;
+      console.log(this.cats)
+    });
+    // load Location
+    let locationObj = <any>locationsJson;
+    this.locations = Object.keys(locationObj).map(function(locationIndex) {
+      let location = locationObj[locationIndex];
+      return location;
+    });
+  }
+
+  mouseenterLevel1(event){
+    let catEl = document.getElementsByClassName("current-level-1");
+    for (let i = 0; i < catEl.length; i++){
+      catEl[i].children[1].classList.remove("show");
+      catEl[i].children[0].classList.remove("active");
+      catEl[i].classList.remove("current-level-1");
+    }
+    event.currentTarget.classList.add("active");
+    let newEl = event.currentTarget.parentElement;
+    newEl.classList.add("current-level-1");
+    newEl.children[1].classList.add("show");
+  }
+
+  mouseenterLevel2(event){
+    let oldEl = document.getElementsByClassName("level-2");
+    for (let i = 0; i < oldEl.length; i++){
+      oldEl[i].classList.remove("active");
+    }
+    let curEl = document.getElementsByClassName("current-level-2");
+    for (let i = 0; i < curEl.length; i++){
+      curEl[i].classList.add("active");
+    }
+    let newEl = event.currentTarget;
+    newEl.classList.add("active");
+  }
+
+  clickLevel1(){
+    return false;
+  }
+
+  clickSubCategory(event){
+    let oldEl = document.getElementsByClassName("current-level-2");
+    for (let i = 0; i < oldEl.length; i++){
+      oldEl[i].classList.remove("active");
+    }
+    let curEl = event.currentTarget;
+    curEl.classList.add("active");
+    curEl.classList.add("current-level-2");
+    this.category = curEl.parentElement.parentElement.parentElement.children[0].innerHTML;
+    this.subCategory = curEl.innerHTML;
+  }
+
+  clickProvince(event){
+    let oldEl = document.getElementsByClassName("current-level-2");
+    for (let i = 0; i < oldEl.length; i++){
+      oldEl[i].classList.remove("active");
+    }
+    let curEl = event.currentTarget;
+    curEl.classList.add("active");
+    curEl.classList.add("current-level-2");
+    this.location = curEl.parentElement.parentElement.parentElement.children[0].innerHTML;
+    this.province = curEl.innerHTML;
   }
 
 }
