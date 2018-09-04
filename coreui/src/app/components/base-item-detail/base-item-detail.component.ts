d618bffeb1edd8ae430ebd8bdd5fd037b496f6db
tramnguyen <tramnguyennt2@gmail.com>
2018-09-04 16:11:41 +0700

    thêm component base-item-detail

---
+++ coreui/src/app/components/base-item-detail/base-item-detail.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+
+@Component({
+  selector: 'app-base-item-detail',
+  templateUrl: './base-item-detail.component.html',
+  styleUrls: ['./base-item-detail.component.scss']
+})
+export class BaseItemDetailComponent implements OnInit {
+
+  id: string;
+  constructor(private route: ActivatedRoute) {
+    this.route.queryParams.subscribe(params => {
+      this.id = params.id;
+    });
+  }
+
+  ngOnInit() {
+    let sidebarEl = document.getElementsByClassName("sidebar-lg-show");
+    for (let i = 0; i < sidebarEl.length; i++) {
+      sidebarEl[i].classList.remove("sidebar-lg-show");
+    }
+  }
+
+}
