22cb8ff56f7333046701a87406da1229c590ffc0
huyentk <huyentk1296@gmail.com>
2018-09-13 17:14:43 +0700

    handle collaborative filtering

--- route.js
+++ route.js
@@ -1,6 +1,5 @@
 let express = require("express");
 let router = express.Router();
-const kafka = require("kafka-node");
 const ContentBasedRecommender = require("content-based-recommender");
 const content_based = new ContentBasedRecommender({
     minScore: 0.1,
@@ -9,25 +8,6 @@ const content_based = new ContentBasedRecommender({
 const nano = require("nano")("http://huyentk:Huyen1312@localhost:5984");
 const db = nano.use("advertisement");
 const recommender = require("recommender");
-
-// router.post("/catch-event", function(req, res, next) {
-//   let data = req.body;
-//   console.log(data);
-//   let Producer = kafka.Producer,
-//     KeyedMessage = kafka.KeyedMessage,
-//     client = new kafka.Client(),
-//     producer = new Producer(client),
-//     payloads = [
-//       { topic: "behavior", messages: JSON.stringify(data), partition: 0 }
-//     ];
-//   producer.on("ready", function() {
-//     producer.send(payloads, function(err, data) {
-//       console.log("Response from kafka server");
-//       console.log(data);
-//     });
-//   });
-//   producer.on("error", function(err) {});
-// });
 
 router.get("/content-based/:id", function (req, res, next) {
     let documents = [];
@@ -51,36 +31,84 @@ router.get("/content-based/:id", function (req, res, next) {
                 0,
                 5
             );
-            console.log(similarDocuments);
             res.send(similarDocuments);
         });
 });
 
-router.get("/cf", function (req, res, next) {
-    let inputMatrix = [
-        [1, 0, 2, 0, 0, 1],
-        [0, 0, 4, 2, 0, 0],
-        [3, 5, 0, 4, 4, 3],
-        [0, 4, 1, 0, 3, 0],
-        [0, 0, 2, 5, 4, 3],
-        [5, 0, 0, 0, 2, 0],
-        [0, 4, 3, 0, 0, 0],
-        [0, 0, 0, 4, 0, 2],
-        [5, 0, 4, 0, 0, 0],
-        [0, 2, 3, 0, 0, 0],
-        [4, 1, 5, 2, 2, 4],
-        [0, 3, 0, 0, 5, 0]
-    ];
-    let movieIndex = 0;
-    let userIndex = 4;
-    let predictedRating = recommender.getRatingPrediction(
-        inputMatrix,
-        movieIndex,
-        userIndex,
-        predictedRating => {
-            console.log(predictedRating);
+router.get("/cf/:id", function (req, res, next) {
+    let documents, user_arr, item_arr, rating_arr = [];
+    let user_idx = 0;
+    const q = {
+        selector: {
+            type: {$eq: "rating"}
         }
-    );
-    res.send(predictedRating);
+    };
+    db.find(q)
+        .then(docs => {
+            for (let i = 0; i < docs.docs.length; i++) {
+                let obj = {
+                    user_id: docs.docs[i].user_id,
+                    item_id: docs.docs[i].item_id,
+                    rating: docs.docs[i].rating
+                };
+                documents.push(obj);
+            }
+            for (let i = 0; i < documents.length; i++) {
+                if (item_arr.indexOf(documents[i].item_id) > -1) {
+                    continue;
+                }
+                item_arr.push(documents[i].item_id);
+            }
+            for (let i = 0; i < documents.length; i++) {
+                if (user_arr.includes(documents[i].user_id)) {
+                    continue;
+                }
+                user_arr.push(documents[i].user_id);
+            }
+            user_idx = user_arr.indexOf(req.params.id);
+            if (user_idx <= -1) {
+                res.send("Not found user");
+                return;
+            }
+            for (let i = 0; i < documents.length; i++) {
+                let item_idx = item_arr.indexOf(documents[i].item_id);
+                let user_idx = user_arr.indexOf(documents[i].user_id);
+                rating_arr.push([user_idx, item_idx, documents[i].rating]);
+            }
+        })
+        .then(() => {
+            let inputMatrix = new Array(user_arr.length);
+            for (let i = 0; i < inputMatrix.length; i++) {
+                inputMatrix[i] = new Array(item_arr.length);
+            }
+            for (let j = 0; j < rating_arr.length; j++) {
+                inputMatrix[rating_arr[j][0]][rating_arr[j][1]] = rating_arr[j][2];
+            }
+            inputMatrix.filter(function (arr) {
+                for (let k = 0; k < arr.length; k++) {
+                    if (arr[k] === undefined) arr[k] = 0;
+                }
+            });
+            recommender.getTopCFRecommendations(inputMatrix, user_idx, {limit: 5},
+                recommendations => {
+                    for (let i = 0; i < recommendations.length; i++) {
+                        recommendations[i].itemId = item_arr[recommendations[i].itemId];
+                    }
+                    res.send(recommendations);
+                }
+            );
+        });
+
+    // let inputMatrix = [
+    //   [0, 0, 0, 2, 2, 3, 0],
+    //   [0, 1, 3, 0, 0, 0, 0],
+    //   [0, 1, 0, 1, 0, 1, 0],
+    //   [2, 0, 0, 4, 0, 1, 1],
+    //   [3, 0, 2, 0, 2, 0, 0]
+    // ];
+    // recommender.getTopCFRecommendations(inputMatrix, 0, recommendations => {
+    //   res.send(recommendations);
+    // });
 });
+
 module.exports = router;
