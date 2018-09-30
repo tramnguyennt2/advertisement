649a8a8660b20d13d8444d16b29e0d3af7abd600
tramnguyen <tramnguyennt2@gmail.com>
2018-09-30 22:38:26 +0700

    Sửa format số tiền

--- coreui/src/app/components/item/item.component.ts
+++ coreui/src/app/components/item/item.component.ts
@@ -8,8 +8,25 @@ import { Component, Input, OnInit } from "@angular/core";
 export class ItemComponent implements OnInit {
   @Input()
   item;
+  price = '';
   constructor() {}
 
   ngOnInit() {
+    this.price = this.formatPrice(this.item.price);
+  }
+
+  formatPrice(price){
+    var price = price;
+    let arr = [];
+    while (price.length > 3) {
+      arr.push(price.slice(-3));
+      price = price.slice(0, price.length - 3);
+    }
+    arr.push(price);
+    let newPrice = '';
+    arr.map(str => {
+      newPrice = ',' + str + newPrice;
+    });
+    return newPrice.slice(1);
   }
 }
